5493f2beda798cf494f6b0a8ed3f589280b5095c
Riya Negi <riyanegi221b@gmail.com>
2023-09-05 10:30:07 +0530

    Add home page route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
+# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   #GET /about
   get "about", to:"about#index"
-  # Defines the root path route ("/")
-  # root "articles#index"
+
+  #GET homepage
+  root to:"home#index"
+
 end
